26d140f8ceb35fa6084b16662a967f8b0651ed4a
Mikhail Koltsov <m.kolcov@altuera.com>
2023-04-29 20:03:49 +0300

    feat: add react lazy, suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import { Counter } from "./components/Counter"
 import './index.scss';
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
 
 export const App = () => {
   return (
     <div className="app">
-      SOME TEXT
-      <Counter />
+      <Link to={'/'}>Main page</Link>
+      <Link to={'/about'}>About</Link>
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync/>}/>
+          <Route path={'/'} element={<MainPageAsync/>}/>
+        </Routes>
+      </Suspense>
     </div>
   )
 }
