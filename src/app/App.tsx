39669f16f3119956fb56cc4488bd4ac54782f4cf
Mikhail Koltsov <m.kolcov@altuera.com>
2023-07-11 23:22:26 +0300

    feat: add app router, router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,7 @@ import { useTheme } from "./providers/ThemeProvider/lib/useTheme";
 import { classNames } from "../shared/lib/classNames/classNames";
 import { AboutPage } from "pages/AboutPage";
 import MainPage from "pages/MainPage/ui/MainPage";
+import { AppRouter } from "./providers/router";
 
 export const App = () => {
 const { theme, toggleTheme } = useTheme();
@@ -16,12 +17,7 @@ const { theme, toggleTheme } = useTheme();
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Main page</Link>
       <Link to={'/about'}>About</Link>
-      <Suspense fallback={<div>loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage/>}/>
-          <Route path={'/'} element={<MainPage/>}/>
-        </Routes>
-      </Suspense>
+      <AppRouter/>
     </div>
   )
 }
